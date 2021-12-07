4de8b42db0b2e15ad10a41f7b1ab7aba10a659bd
Isac Eduard <edi.isac16@gmail.com>
2021-12-07 13:20:40 +0200

    Istoric + Zone noi

---
+++ REACT-APP/my-app/src/components/signup.component.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+export function SignUp() {
+
+        return (
+          <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={"/sign-in"}>AMBROZIA</Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-in"}>Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-up"}>Register</Link>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+          </nav>
+           
+            <form>
+                <h3>Register</h3>
+
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Register</button>
+               
+            </form></div>
+        );
+    }
+export default SignUp;
